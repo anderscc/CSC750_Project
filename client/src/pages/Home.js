e25619481abc3e991c7bfd04f2bae52722dea87d
Calvin Anderson <andersoncalvin289@gmail.com>
2022-10-05 10:28:04 -0500

    add return to home and previous on viewSchedule page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,18 +10,25 @@ class Home extends Component {
     render(){
         return(
             <div className={'container-fluid'}>
-                <section className="home" id="home">
-                <h1> Welcome to the Graduate Assistant Systems Scheduler (GASS)!</h1>
-                <p> Here we aim to solve your problems of scheduling conflicts between
-                    busy schedules of Graduate Assistants and Teaching Assistants as well
-                    as meeting the needs of faculty and course requirements. This software also
-                    schedules the necessary time required for lab meetings while respecting the time
-                    constraints of half and full-time graduate/teaching assistants.
-                </p>
-                <p> This button below tests the next page feature to see if it is working</p>
-                <button onClick={this.nextStep.bind(this)}>Next Page</button>
+                <div>
+                    <h1> Welcome to the Graduate Assistant Systems Scheduler (GASS)!</h1>
+                    <p> Here we aim to solve your problems of scheduling conflicts between
+                        busy schedules of Graduate Assistants and Teaching Assistants as well
+                        as meeting the needs of faculty and course requirements. This software also
+                        schedules the necessary time required for lab meetings while respecting the time
+                        constraints of half and full-time graduate/teaching assistants.
+                    </p>
+                </div>
+                <div>
+                    <p> This button below tests the next page feature to see if it is working</p>
+                    <button onClick={this.nextStep.bind(this)}>Next Page</button>
+                </div>
+                <div>
+                    <p> If you have previously generated a schedule, select which schedule you are
+                        trying to use here to view previous data/schedules</p>
+                    <button onClick = {this.previousData.bind(this)}>Previous Data</button>
+                </div>
                 
-                </section>
             </div>
         )
     }
@@ -30,6 +37,20 @@ class Home extends Component {
     nextStep() {
         this.props.nextStep();
       }
+    
+    //function to allow user to retrieve data from previously generated schedule
+    previousData(){
+        var data = {
+            studentName: this.props.values.studentName,
+            classTimes: this.props.values.classTimes,
+            hoursAvail: this.props.values.hoursAvail,
+            coursePref:this.props.values.coursePref,
+            facultyPref:this.props.values.facultyPref,
+            officeHours: this.props.values.officeHours,
+          }
+      
+          this.props.saveValues(data);
+    }
 }
 
 export default Home;
