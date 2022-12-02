2e1163788c8ee79c0d602cab969c691ccd068259
Calvin Anderson <andersoncalvin289@gmail.com>
2022-12-01 20:26:50 -0600

    tasks to do

--- client/src/pages/ViewSchedule.js
+++ client/src/pages/ViewSchedule.js
@@ -1,6 +1,21 @@
 
 import React, { Component } from 'react';
-  
+
+//it'll be one table
+//get the data from api probably in array format
+//go through each row of the array and display in the table
+//[CSC701,Godwin Ekuma,...etc] dummy data array
+
+//Needs to be a button that 
+//Automatic download as well as click download
+
+
+//<button r"../schedule.csv" - relative path
+//take array file and convert into csv file in front-end
+
+// var array = [ "geeks", "4", "geeks" ];
+// var csv = array.toString();
+// document.write(csv);
 class ViewSchedule extends Component {
   render(){
     return (
