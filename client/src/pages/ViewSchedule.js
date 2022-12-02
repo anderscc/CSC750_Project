af9d905abc6154eeee558247dc3f0f529c9a9758
Calvin Anderson <andersoncalvin289@gmail.com>
2022-12-01 20:58:47 -0600

    dummy data

--- client/src/pages/ViewSchedule.js
+++ client/src/pages/ViewSchedule.js
@@ -16,6 +16,34 @@ import React, { Component } from 'react';
 // var array = [ "geeks", "4", "geeks" ];
 // var csv = array.toString();
 // document.write(csv);
+
+
+//id conflicts semester year
+//
+
+//Assignment ID, GATAid, semYr, ScheduleNum, MeetTimes, Courses
+//[
+// [1,1,"Fall22","CALEB B.",""Mr. Incredible,CSC 125.002,M 3:00 - 4:30,16,16,2,4 
+// [1,2,"Fall22","WENYU Z.","Jack Jack,CSC 197.002,T 3:00 - 4:30,6,6,3,5 
+// [1,3,"Fall22","GODWIN E.","Ms. Incredible,CSC 226.002,W 5:30 - 6:30,6.5,6.5,2.5,4.5 
+// [1,4,"Fall22","OLUWATOBI A.","Dash,CSC 121.002,R 4:05 - 6:05,16.5,16.5,1.5,3.5 
+// [1,5,"Fall22","Dash","None",CSC 121.001,R 2:00 - 4:00,None,None,None,1.5 
+// [1,6,"Fall22","Ms. Incredible","None",CSC 226.001,W 4:00 - 5:00,None,None,None,2.5 
+// [1,7,"Fall22","Jack Jack","None",CSC 197.001,T 1:00 - 2:30,None,None,None,3 
+// [1,8,"Fall22","Mr. Incredible","None",CSC 125.001,M 1:00 - 2:30,None,None,None,2 
+// [1,9,"Fall22","CALVIN A.","None",CSC 799.001,M 11:00 - 12:00,16,16,2,None 
+// [1,10,"Fall22","OLUWATOBI A.","None",CSC 755.001,M 1:00 - 3:00,14.5,14.5,2,None 
+// [1,11,"Fall22","GODWIN E.","None",CSC 750.001,M 5:00 - 7:30,4.5,4.5,2,None 
+// [1,12,"Fall22","WENYU Z.","None",CSC 747.001,R 10:00 - 12:00,4,4,2,None 
+// [1,13,"Fall22","CALVIN A.","None",CSC 790.001,TW 11:00 - 12:00,14,14,2,None 
+// [1,14,"Fall22","CALEB B.","None",CSC 765.001,F 1:00 - 2:30,14,14,2,None 
+// [1,15,"Fall22","WENYU Z.","None",CSC 746.001,T 4:00 - 5:15,2,2,2,None 
+// [1,16,"Fall22","GODWIN E.","None",CSC 745.001,W 9:00 - 10:15,2.5,2.5,2,None 
+// [1,17,"Fall22","OLUWATOBI A.","None",CSC 742.001,TR 3:30 - 4:45,12.5,12.5,2,None 
+// [1,18,"Fall22","CALVIN A.","None",CSC 737.001,T 9:00 - 10:00,12,12,2,None 
+// [1,19,"Fall22","CALEB B.","None",CSC 736.001,F 09:00 - 11:00,12,12,2,None]
+// ]
+
 class ViewSchedule extends Component {
   render(){
     return (
