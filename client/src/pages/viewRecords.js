65892205b0a87c9b4d0272825bcd6b9ccbdc01ba
Wenyu Zhao <zhao926@;ive.missouristate.edu>
2022-12-05 20:38:25 -0600

    1

--- client/src/pages/viewRecords.js
+++ client/src/pages/viewRecords.js
@@ -241,6 +241,7 @@ const ViewRecords = () => {
         });
         console.log("clicked edit",record.id,isEditing)
         setEditingKey(record.id);
+        console.log(editingKey)
         /*TODO: Need to be connect to the API*/
     };
     const cancel = () => {
@@ -324,8 +325,9 @@ const ViewRecords = () => {
             title: 'Action',
             key: 'action',
             render: (_, record) => {
+                console.log("isEditing",record.id,editingKey)
                 const editable = isEditing(record);
-                console.log("editable",editable)
+                console.log("editable",editable,editingKey)
                 return editable ? (
                     <Space size="middle">
                         <Typography.Link
